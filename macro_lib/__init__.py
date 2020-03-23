79e91f98c358f998c6f97b34d1320637dc592d6a
Peter Zhao <peterzhao204@gmail.com>
2020-03-23 17:26:06 -0400

    Add code snippet to check pre-import requirements

--- macro_lib/__init__.py
+++ macro_lib/__init__.py
@@ -1,2 +1,10 @@
+try:
+	import numpy
+except:
+	raise ImportError("numpy is needed for the library to function")
+try:
+	import pandas
+except:
+	raise ImportError("pandas is needed for the library to function")
 import assist
 import plot_helper
