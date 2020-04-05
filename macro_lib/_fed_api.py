cfeb4ebdc40a4bcb16aa584d95e5d3d5c17c08f0
Peter Zhao <peterzhao204@gmail.com>
2020-04-05 16:48:41 -0400

    update _fed_api.py

--- macro_lib/_fed_api.py
+++ macro_lib/_fed_api.py
@@ -1,8 +1,7 @@
 '''
-Provides functionality which allows easy access to the macroeconomic data through 
+A simple interface which enables easy access to the macroeconomic data through 
 the Federal Reserve Bank of St. Louis API
 '''
-from macro_lib import _assist
 from urllib import request
 from urllib import error
 from urllib import parse
@@ -14,23 +13,13 @@ class DataQuery:
     '''
     __API_KEY__: the unique api_key for our application; this is needed for pulling GET Request
     __HTTP__:    Path to the source on Web Server
-    __current__: the most recent requested list of categories 
-    __history__: a stack that allows user to go back to their previously request list of categories
+    __current__: the most recent requested data 
+    __history__: a stack that allows user to go back to their previously request data
     '''
     def __init__(self):
         self.__API_KEY__ = "4f96326c4128a0f9e5a951bd674b6b61"
         self.__HTTP__ = "https://api.stlouisfed.org/"
-        self.__CATEGORIES__ = {
-            'Money, Banking, & Finance' : 32991, 
-            'Population, Employment, & Labor Markets' : 10,
-            'National Accounts' : 32992,
-            'Production & Business Activity' : 1,
-            'Prices' : 32455,
-            'International Data' : 32263,
-            'US Regional Data' : 3008 ,
-            'Academic Data' : 33060
-        }
-        self.__current__ = self.__CATEGORIES__
+        self.__current__ = None
         self.__history__ = list()  
 
     '''
@@ -98,60 +87,78 @@ class DataQuery:
         return url
 
     '''
-    return a list of category user can navigate to
+    get the last requested data
     '''
-    def get(self):
-        return sorted(self.__current__)
-
-    '''
-    Navigate to one of the categories of data
-  
-    Parameters:
-        * selection:  the index of the category the user choose to navigate to
-        * assist:     optional parameter, if true get the argument through prompts
-   
-    Returns:
-        * the selected category's child
-
-    Example:
-        go_to('US Regional Data') should return:
-        [States, BEA Regions, BLS Regions, Federal Reserve Districts, Freddie Mac Regions]
-        in sorted order
-    '''
-    def go_to(self, selection = None, assist = False):
-        if assist:
-            prompt = "Please choose from one of the following categories to navigate into:\n--------------------------------------------------------------------\n"
-            categories = list(self.get())
-            for category in categories:
-                prompt += (category + "\n")
-            selection = _assist._ask_for_one_(prompt, type(""))
-            if (selection == None):
-                return
-        elif selection == None:
-            raise ValueError("selection must be either a string or an index")
-        if type(selection) == type(5):
-            if selection - 1 >= len(self.get()):
-                raise IndexError("Attempt to go to a non-exisiting category")
-            selection = self.get()[selection - 1]
-        elif type(selection) != type(""):
-            raise ValueError("selection must be either a string or an index")
-        if selection not in self.__current__.keys():
-            raise ValueError("Attempt to go to a non-exisiting category")
-        url = self._parse_request({'category id' : self.__current__[selection]}, "fred/category/children")        
-        try:
-            data = request.urlopen(url).read()
-            data = json.loads(data)['categories']
-        except:
-            raise ConnectionError("HTTP Request was not successful")
-        self.__history__.append(self.__current__)
-        self.__current__ = dict(zip([e['name'] for e in data], [d['id'] for d in data]))
-        return self.get()
+    def _get(self):
+        if (self.__current__ == 'None'):
+            raise ValueError("You have not requested any data")
+        return self.__current__
 
     '''
     Navigate back and return the category's parent
     '''
-    def go_back(self):
+    def _go_back(self):
         if len(self.__history__) != 0:
             self.__current__ = self.__history__.pop()
         return self.get()
+
+    '''
+    Get the observation or data values for an economic data series and convert them
+    into a pandas dataframe
+  
+    Parameters:
+        * id:  the series_id, which can be found in the FED's site
    
+    Returns:
+        * the entire obervation
+    '''
+    def _fetch_all(self, id):
+        if type(id) != type(""):
+            raise ValueError("id must be a string")
+        url = self._parse_request({'series_id' : id}, "fred/series/observations")        
+        try:
+            data = request.urlopen(url).read()
+        except:
+            raise ConnectionError("HTTP Request was not successful")
+        data = json.loads(data)['observations']
+        if data == {}:
+            raise ValueError("id is invalid")
+        if (self.__current__ != None):
+            self.__history__.append(self.__current__)
+        df = pd.DataFrame(dict(zip([d['date'] for d in data], [v['value'] for v in data])))
+        self.__history__.append(self.__current__)
+        self.__current__ = df
+        return __current__
+
+    '''
+    Get the observation or data values for an economic data series from a given
+    starting date to a given ending date and convert them into a pandas dataframe
+  
+    Parameters:
+        * id: the series_id, which can be found in the FED's site
+        * obs_start: the start date of the time series in the format YYYY-MM-DD
+        * obs_end: the end date of the time series in the format YYYY-MM-DD
+   
+    Returns:
+        * observations from a certain start date to an end date
+    '''
+    def _fetch_by_date(self, id, obs_start, obs_end):
+        if (type(id) != type ("") or type(obs_start) != type("") or type(obs_end) != type("")):
+            raise ValueError("id must be a string")
+        url = self._parse_request({'series_id' : id, 'observation_start': obs_start, 'observation_end' : obs_end})
+        try:
+            data = request.urlopen(url).read()
+        except:
+            raise ConnectionError("HTTP Request was not successful")
+        if data == {}:
+            raise ValueError("the query returns nothing")
+        data = json.loads(data)['observations']
+        if (self.__current__ != None):
+            self.__history__.append(self.__current__)
+        if (len(data['date'] == 1)):
+            df = data['value']
+        else:
+            df = pd.DataFrame(dict(zip([d['date'] for d in data], [v['value'] for v in data])))
+        self.__current__ = df
+        return __current__
+
